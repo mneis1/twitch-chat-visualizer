ead5e4ba186f34c5549d5023034bdb6b64f9b9f4
Stuart <36939648+stuart5@users.noreply.github.com>
2018-04-29 02:38:17 -0400

    My function
    Here it is, reorder the json that it gives, but it has the info u want

--- text_interpreter/pop_text_compiler/popular_text.py
+++ text_interpreter/pop_text_compiler/popular_text.py
@@ -1,9 +1,11 @@
 import nltk
 from nltk.collocations import *
 import requests
+import collections
 import string
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
+import json
 
 
 class popText:
@@ -56,7 +58,18 @@ class popText:
         pass
 
 
-x = popText()
-a = [{'username': 'spiderinqz', 'message': u'F', 'channel': '#overwatchleague'}, {'username': 'the_mathcat', 'message': u'100', 'channel': '#overwatchleague'}, {'username': 'pepsidude555', 'message': u'100', 'channel': '#overwatchleague'}, {'username': 'lavow', 'message': u'SPAM SMOrc THIS SMOrc BOSS SMOrc TO SMOrc MOURN SMOrc SHANGHAI\u2019S SMOrc loss', 'channel': '#overwatchleague'}, {'username': 'phaaedra', 'message': u'100', 'channel': '#overwatchleague'}, {'username': 'acalacaboo', 'message': u'F', 'channel': '#overwatchleague'}, {'username': 'danzilla132', 'message': u'100', 'channel': '#overwatchleague'}, {'username': 'irrelavantstreams', 'message': u'Hey i got 100 tokens', 'channel': '#overwatchleague'}, {'username': 'bazzlover69', 'message': u'LOL', 'channel': '#overwatchleague'}]
-c=x.collate_data(a)
-print(c)
+#x = popText()
+#a = [{'username': 'spiderinqz', 'message': u'F', 'channel': '#overwatchleague'}, {'username': 'the_mathcat', 'message': u'100', 'channel': '#overwatchleague'}, {'username': 'pepsidude555', 'message': u'100', 'channel': '#overwatchleague'}, {'username': 'lavow', 'message': u'SPAM SMOrc THIS SMOrc BOSS SMOrc TO SMOrc MOURN SMOrc SHANGHAI\u2019S SMOrc loss', 'channel': '#overwatchleague'}, {'username': 'phaaedra', 'message': u'100', 'channel': '#overwatchleague'}, {'username': 'acalacaboo', 'message': u'F', 'channel': '#overwatchleague'}, {'username': 'danzilla132', 'message': u'100', 'channel': '#overwatchleague'}, {'username': 'irrelavantstreams', 'message': u'Hey i got 100 tokens', 'channel': '#overwatchleague'}, {'username': 'bazzlover69', 'message': u'LOL', 'channel': '#overwatchleague'}]
+#c=x.collate_data(a)
+#print(c)
+
+def biggestCommenters(listOfDictsIn):
+    usersList = [li['username'] for li in listOfDictsIn] #makes list of all the usernames
+    #for x in listOfDictsIn
+
+    counter = collections.Counter(usersList) #now make it into a dictionary of usernames as keys, fields are count, from highest to lowest
+
+    return json.loads(json.dumps(counter)) #loads to desired dict json representation
+
+
+#biggestCommenters([{'username': 'spiderinqz', 'message': u'F', 'channel': '#overwatchleague'}, {'username': 'the_mathcat', 'message': u'100', 'channel': '#overwatchleague'}, {'username': 'pepsidude555', 'message': u'100', 'channel': '#overwatchleague'}, {'username': 'lavow', 'message': u'SPAM SMOrc THIS SMOrc BOSS SMOrc TO SMOrc MOURN SMOrc SHANGHAI\u2019S SMOrc loss', 'channel': '#overwatchleague'}, {'username': 'phaaedra', 'message': u'100', 'channel': '#overwatchleague'}, {'username': 'the_mathcat', 'message': u'F', 'channel': '#overwatchleague'}, {'username': 'danzilla132', 'message': u'100', 'channel': '#overwatchleague'}, {'username': 'irrelavantstreams', 'message': u'Hey i got 100 tokens', 'channel': '#overwatchleague'}, {'username': 'bazzlover69', 'message': u'LOL', 'channel': '#overwatchleague'}])
