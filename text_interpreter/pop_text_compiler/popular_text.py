0866a89685880d2765aa3eca0d563e918fed81b6
tazer101 <deetunemylife@gmail.com>
2018-04-29 00:56:59 -0400

    finished algorithm

--- text_interpreter/pop_text_compiler/popular_text.py
+++ text_interpreter/pop_text_compiler/popular_text.py
@@ -4,13 +4,14 @@ import requests
 import string
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
+import json
 
 
 class popText:
     def __init__(self):
         self.archiveMessages = []
 
-        self.occurances_threshold = 3
+        self.occurances_threshold = 2
         self.highest_ngram_pmi_lim = 10
 
     def collate_data(self, words):
@@ -18,6 +19,7 @@ class popText:
         #trigram_measures = nltk.collocations.TrigramAssocMeasures()
 
         tempColloList = []
+        tempDictList = []
 
         # makes a list of words from a json of words
         #words = json_to_words(json_data)
@@ -28,14 +30,14 @@ class popText:
         tokenizer = RegexpTokenizer(r'\w+')
 
         for i in self.archiveMessages:
-            message=i.get('message').lower()
-            tokenizer.tokenize(message)
+            message=i.get('message').lower().split()
+            #tokenizer.tokenize(message)
             tempColloList.extend(message)
 
         #TODO:incorperate stop word trimming
         #TODO: turn words to topic (dogs to dog)
 
-        finder = BigramCollocationFinder.from_words(words)
+        finder = BigramCollocationFinder.from_words(tempColloList)
         finder.apply_freq_filter(self.occurances_threshold)
 
         b=finder.nbest(bigram_measures.pmi, self.highest_ngram_pmi_lim)
@@ -45,9 +47,13 @@ class popText:
         #
 
         for k, v in finder.ngram_fd.items():
-            print(k,v)
+            tempDict = {}
+            tempDict["topic"] = " ".join(k)
+            tempDict["occurance"] = v
+            tempDictList.append(tempDict.copy())
 
-        return b
+        return json.dumps(tempDictList)
+
 
         # TODO: may need code here that calls push_data
 
@@ -56,7 +62,7 @@ class popText:
         pass
 
 
-x = popText()
-a = [{'username': 'spiderinqz', 'message': u'F', 'channel': '#overwatchleague'}, {'username': 'the_mathcat', 'message': u'100', 'channel': '#overwatchleague'}, {'username': 'pepsidude555', 'message': u'100', 'channel': '#overwatchleague'}, {'username': 'lavow', 'message': u'SPAM SMOrc THIS SMOrc BOSS SMOrc TO SMOrc MOURN SMOrc SHANGHAI\u2019S SMOrc loss', 'channel': '#overwatchleague'}, {'username': 'phaaedra', 'message': u'100', 'channel': '#overwatchleague'}, {'username': 'acalacaboo', 'message': u'F', 'channel': '#overwatchleague'}, {'username': 'danzilla132', 'message': u'100', 'channel': '#overwatchleague'}, {'username': 'irrelavantstreams', 'message': u'Hey i got 100 tokens', 'channel': '#overwatchleague'}, {'username': 'bazzlover69', 'message': u'LOL', 'channel': '#overwatchleague'}]
-c=x.collate_data(a)
-print(c)
+#x = popText()
+#a = [{'username': 'spiderinqz', 'message': u'F', 'channel': '#overwatchleague'}, {'username': 'the_mathcat', 'message': u'100', 'channel': '#overwatchleague'}, {'username': 'pepsidude555', 'message': u'100', 'channel': '#overwatchleague'}, {'username': 'lavow', 'message': u'SPAM SMOrc THIS SMOrc BOSS SMOrc TO SMOrc MOURN SMOrc SHANGHAI\u2019S SMOrc loss', 'channel': '#overwatchleague'}, {'username': 'phaaedra', 'message': u'100', 'channel': '#overwatchleague'}, {'username': 'acalacaboo', 'message': u'F', 'channel': '#overwatchleague'}, {'username': 'danzilla132', 'message': u'100', 'channel': '#overwatchleague'}, {'username': 'irrelavantstreams', 'message': u'Hey i got 100 tokens', 'channel': '#overwatchleague'}, {'username': 'bazzlover69', 'message': u'LOL', 'channel': '#overwatchleague'}]
+#c=x.collate_data(a)
+#print(c)
